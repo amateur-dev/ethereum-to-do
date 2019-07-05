4b0a43cd8c331d48980de0c98c9d8640eeb55664
amateu-dev <dipesh.sukhani@gmail.com>
2019-07-05 12:15:37 +0800

    updated the contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,9 +3,23 @@ pragma solidity ^0.5.0;
 contract TodoList {
     // state variables
     
-    address public user;
     uint8 public taskCount = 0;
 
+    struct Task {
+        uint8 id;
+        string content;
+        uint8 status;
+    }
 
+    mapping (uint=>Task) public tasks;
 
+    function createTask (string memory _content) public {
+        taskCount ++;
+        Task memory new_task = Task({
+            id: taskCount,
+            content: _content,
+            status: 0
+        });
+        tasks[taskCount] = new_task;
+    }
 }
